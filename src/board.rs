e666b4759d0a3850042a9859ccef76bc7ebf1731
deagahelio <16138291+deagahelio@users.noreply.github.com>
2019-11-05 15:57:33 -0300

    Variable board size

--- src/board.rs
+++ src/board.rs
@@ -3,38 +3,39 @@ use ggez::graphics::{DrawMode, Mesh, Rect};
 use anyhow::{Result, anyhow};
 use crate::piece::{PIECE_COLORS, PIECE_SHAPES, Rotation, Shape, rotate_piece};
 
-pub const BOARD_WIDTH: usize = 10;
-pub const BOARD_HEIGHT: usize = 20;
-
 pub struct Board {
     pos: mint::Point2<f32>,
     cell_size: f32,
-    grid: [Option<Shape>; BOARD_WIDTH * BOARD_HEIGHT],
+    width: usize,
+    height: usize,
+    grid: Vec<Option<Shape>>,
 }
 
 impl Board {
-    pub fn new(pos: mint::Point2<f32>, cell_size: f32) -> Self {
+    pub fn new(pos: mint::Point2<f32>, cell_size: f32, width: usize, height: usize) -> Self {
         Self {
             pos,
             cell_size,
-            grid: [None; BOARD_WIDTH * BOARD_HEIGHT],
+            width,
+            height,
+            grid: vec![None; width * height],
         }
     }
 
-    pub fn index(x: usize, y: usize) -> Result<usize> {
-        if x >= BOARD_WIDTH || y >= BOARD_HEIGHT {
+    pub fn index(&self, x: usize, y: usize) -> Result<usize> {
+        if x >= self.width || y >= self.height {
             Err(anyhow!("out of bounds index"))
         } else {
-            Ok(y * BOARD_WIDTH + x)
+            Ok(y * self.width + x)
         }
     }
 
     pub fn get(&self, x: usize, y: usize) -> Result<Option<Shape>> {
-        Ok(self.grid[Board::index(x, y)?])
+        Ok(self.grid[self.index(x, y)?])
     }
 
     pub fn set(&mut self, x: usize, y: usize, new: Option<Shape>) -> Result<Option<Shape>> {
-        let index = Board::index(x, y)?;
+        let index = self.index(x, y)?;
         let old = self.grid[index];
         self.grid[index] = new;
         Ok(old)
@@ -45,12 +46,12 @@ impl Board {
         let shape_height = shape.len();
         let shape_width = shape[0].len();
 
-        if x > BOARD_WIDTH - shape_width {
-            x = BOARD_WIDTH - shape_width;
+        if x > self.width - shape_width {
+            x = self.width - shape_width;
         }
 
         let mut y = 0;
-        'drop: while y <= BOARD_HEIGHT - shape_height {
+        'drop: while y <= self.height - shape_height {
             for piece_y in 0..shape_height {
                 let row = &shape[piece_y];
                 for piece_x in 0..shape_width {
@@ -77,17 +78,27 @@ impl Board {
         Ok(())
     }
 
+    pub fn get_rotation(&self, grid_y: usize) -> Option<Rotation> {
+        match self.height - grid_y {
+            4 => Some(Rotation::OneEighty),
+            3 => Some(Rotation::Clockwise),
+            2 => Some(Rotation::Normal),
+            1 => Some(Rotation::CounterClockwise),
+            _ => None,
+        }
+    }
+
     pub fn clear_line(&mut self, line_y: usize) {
         for y in (0..line_y).rev() {
-            for x in 0..BOARD_WIDTH {
+            for x in 0..self.width {
                 self.set(x, y + 1, self.get(x, y).unwrap()).unwrap();
             }
         }
     }
 
     pub fn clear_lines(&mut self) {
-        'lines: for y in 0..BOARD_HEIGHT {
-            for x in 0..BOARD_WIDTH {
+        'lines: for y in 0..self.height {
+            for x in 0..self.width {
                 if self.get(x, y).unwrap().is_none() {
                     continue 'lines;
                 }
@@ -99,8 +110,8 @@ impl Board {
     pub fn click(&self, x: f32, y: f32) -> Option<(usize, usize)> {
         if x > self.pos.x &&
            y > self.pos.y &&
-           x < (self.pos.x + BOARD_WIDTH as f32 * self.cell_size) &&
-           y < (self.pos.y + BOARD_HEIGHT as f32 * self.cell_size) {
+           x < (self.pos.x + self.width as f32 * self.cell_size) &&
+           y < (self.pos.y + self.height as f32 * self.cell_size) {
             let grid_x = (x - self.pos.x) as usize / self.cell_size as usize;
             let grid_y = (y - self.pos.y) as usize / self.cell_size as usize;
 
@@ -128,14 +139,14 @@ impl Board {
         let grid_mesh = Mesh::new_rectangle(
             ctx,
             DrawMode::stroke(1.0),
-            Rect::new(self.pos.x, self.pos.y, self.cell_size * BOARD_WIDTH as f32, self.cell_size * BOARD_HEIGHT as f32),
+            Rect::new(self.pos.x, self.pos.y, self.cell_size * self.width as f32, self.cell_size * self.height as f32),
             (243, 243, 237, 25).into()
         )?;
 
         graphics::draw(ctx, &grid_mesh, ([0.0, 0.0],))?;
 
-        for y in 0..BOARD_HEIGHT {
-            for x in 0..BOARD_WIDTH {
+        for y in 0..self.height {
+            for x in 0..self.width{
                 let pos = [x as f32 * self.cell_size, y as f32 * self.cell_size];
 
                 let block = self.get(x, y).unwrap();
--- src/states/game_state.rs
+++ src/states/game_state.rs
@@ -1,7 +1,7 @@
 use ggez::{self, graphics, event, GameResult, Context};
 use ggez::input::mouse::MouseButton;
-use crate::board::{BOARD_HEIGHT, Board};
-use crate::piece::{Rotation, Shape};
+use crate::board::Board;
+use crate::piece::Shape;
 use crate::generator::PieceGenerator;
 use crate::queue::Queue;
 
@@ -15,20 +15,10 @@ pub struct GameState<G: PieceGenerator> {
 impl<G: PieceGenerator> GameState<G> {
     pub fn new(mut generator: G) -> Self {
         Self {
-            board: Board::new([32.0, 32.0].into(), 32.0),
+            board: Board::new([32.0, 32.0].into(), 32.0, 10, 20),
             queue: Queue::new([384.0, 32.0].into(), 32.0),
             current: generator.get_next(),
             generator,
-        }
-    }
-
-    pub fn get_rotation(&self, grid_y: usize) -> Option<Rotation> {
-        match BOARD_HEIGHT - grid_y {
-            4 => Some(Rotation::OneEighty),
-            3 => Some(Rotation::Clockwise),
-            2 => Some(Rotation::Normal),
-            1 => Some(Rotation::CounterClockwise),
-            _ => None,
         }
     }
 }
@@ -41,7 +31,7 @@ impl<G: PieceGenerator> event::EventHandler for GameState<G> {
     fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         if button == MouseButton::Left {
             if let Some((grid_x, grid_y)) = self.board.click(x, y) {
-                if let Some(rotation) = self.get_rotation(grid_y) {
+                if let Some(rotation) = self.board.get_rotation(grid_y) {
                     if self.board.put(grid_x, rotation, self.current).is_ok() {
                         self.current = self.generator.get_next();
                         self.board.clear_lines();
